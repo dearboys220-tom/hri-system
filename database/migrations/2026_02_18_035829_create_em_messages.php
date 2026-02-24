1e00c2ad370529cb0af119749c739c627240c34c
rafyfakhrizal299 <rafyfakhrizal299@gmail.com>
2026-02-24 12:27:50 +0700

    fixing and adding script for artisan and npm

--- database/migrations/2026_02_18_035829_create_em_messages.php
+++ database/migrations/2026_02_18_035829_create_em_messages.php
@@ -27,6 +27,8 @@ return new class extends Migration
 
             $table->text('message');
 
+            $table->boolean('is_read')->default(false);
+            $table->timestamp('read_at')->nullable();
             
             $table->timestamps();
 
